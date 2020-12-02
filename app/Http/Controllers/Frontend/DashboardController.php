a9b583cc0a49cd146be8f2b1547486db4ce57dce
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2020-12-03 06:09:12 +0700

    add template

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-       return view('frontend.master');
+       return view('frontend.dashboard.index');
     }
 
     /**
