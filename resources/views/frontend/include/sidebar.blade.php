8a6ca9870dd0a7f53f15e9b28101752e967bc4bc
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2020-12-03 06:04:57 +0700

    add template

---
+++ resources/views/frontend/include/sidebar.blade.php
@@ -0,0 +1,13 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="">{{ env('APP_NAME') }}</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+        <li class="active"><a class="nav-link" href=""><i class="fa fa-columns"></i> <span>Dashboard</span></a></li>
+
+      </ul>
+  </aside>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
-
+Route::resource('dashboard', 'Frontend\DashboardController');
 Route::get('/home', 'HomeController@index')->name('home');
