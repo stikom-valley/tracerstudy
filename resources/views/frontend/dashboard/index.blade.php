45e71fffe025fe598f16c3c0af85bfc3f6f605e4
Yuri Dimas Satrio <yuridimas00@gmail.com>
2020-12-14 05:33:07 +0700

    Layout: tambah greeting dashboard

--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <section class="section">
   <div class="section-header">
-    <h1>Dashboard</h1>
+    <h1>Selamat Datang, {{ Auth::user()->name }}!</h1>
   </div>
 
   <div class="section-body">
